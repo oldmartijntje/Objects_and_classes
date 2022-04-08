35e03ab74f7412421f2ea561f93d84bb04d0cb12
oldmartijntje <oldmartijntje@gmai.com>
2022-04-08 11:48:50 +0200

    commit after adding step 1

---
+++ robotarm-python-2021-main/robotarm-python-2021-main/example.py
@@ -0,0 +1,12 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 1')
+
+# Jouw python instructies zet je vanaf hier:
+
+robotArm.moveRightTimes(3)
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
